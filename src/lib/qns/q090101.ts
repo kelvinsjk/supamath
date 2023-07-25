715120c9a23687cec7a0dbd9cb961245cd99560a
Kelvin Soh <kelvinsoh@pop-os.localdomain>
2023-07-26 00:53:45 +0800

    added integral, add spacing for exp

--- src/lib/qns/q090101.ts
+++ src/lib/qns/q090101.ts
@@ -42,7 +42,7 @@ function case1Gen(case1: number,
 	// 0: ln x, 1: x^a + b, 2: ax^2 + bx + c
 	if (case1===0){
 		const ans = math(`\\ln \\lvert \\ln x \\rvert + c`);
-		return [math(`\\frac{1}{x \\ln x}`), ans, ans, '']
+		return [math(`\\int \\frac{1}{x \\ln x} \\; \\mathrm{d}x`), ans, ans, '']
 	}
 	if (case1===1){
 		const xA = `x^{${a1}}`;
@@ -50,7 +50,7 @@ function case1Gen(case1: number,
 			: signCase1===1 ? new Expression(xA, -b1)
 			: new Expression(b1, new Term(-1, xA));
 		const fPrime = a1===2 ? `x` : `x^{${a1-1}}`;
-		const qn = math(`\\frac{${fPrime}}{${fx}}`);
+		const qn = math(`\\int \\frac{${fPrime}}{${fx}} \\; \\mathrm{d}x`);
 		const ans = math(`\\frac{1}{${a1}} \\ln ${modulus(`${fx}`, a1, signCase1)} + c`);
 		return [qn, ans, ans, ''];
 	}
@@ -59,7 +59,7 @@ function case1Gen(case1: number,
 	const fPrime = fx.differentiate()
 	const fPrimeSimplified = fPrime.simplify();
 	const multiple = fPrimeSimplified.coeffs[0].divide(fPrime.coeffs[0]);
-	const qn = math(`\\frac{${fPrimeSimplified}}{${fx}}`);
+	const qn = math(`\\int \\frac{${fPrimeSimplified}}{${fx}} \\; \\mathrm{d}x`);
 	const alwaysPositive = new Fraction(b1).square().minus(new Fraction(4).times(a1).times(c1)).isLessThan(0) ? 1 : 0;
 	const lnTerm = new Term(multiple, `\\ln ${modulus(`${fx}`,2,alwaysPositive)}`);
 	const ans = math(`${lnTerm} + c`);
@@ -74,9 +74,9 @@ function case2Gen(case2: number, a2: number, b2: number, n: number): [string, st
 	// case 2: f'(x) ( f(x) )^n
 	// 0: ln x, 1: x^a + b, 2: trig
 	if (case2===0){
-		const qn = n===1 ? math(`\\frac{\\ln x}{x}`)
-			: n>1 ? math(`\\frac{(\\ln x)^{${n}}}{x}`)
-			: math(`\\frac{1}{x (\\ln x)^{${-n}}}`);
+		const qn = n===1 ? math(`\\int \\frac{\\ln x}{x} \\; \\mathrm{d}x`)
+			: n>1 ? math(`\\int \\frac{(\\ln x)^{${n}}}{x} \\; \\mathrm{d}x`)
+			: math(`\\int \\frac{1}{x (\\ln x)^{${-n}}} \\; \\mathrm{d}x`);
 		const ans = n>0 ? math(`\\frac{(\\ln x)^{${n+1}}}{${n+1}} + c`)
 			: n === -2 ? math(`-\\frac{1}{\\ln x} + c`)
 			: math(`-\\frac{1}{${-n-1}(\\ln x)^{${-n-1}}} + c`);
@@ -87,8 +87,8 @@ function case2Gen(case2: number, a2: number, b2: number, n: number): [string, st
 		const xA = `x^{${a2}}`;
 		const fx = new Expression(xA, b2);
 		const fPrime = a2===2 ? `x` : `x^{${a2-1}}`;
-		const qn = n > 0 ? math(`${fPrime} (${fx})^{${n}}`)
-			: math(`\\frac{${fPrime}}{(${fx})^{${-n}}}`);
+		const qn = n > 0 ? math(`\\int ${fPrime} (${fx})^{${n}} \\; \\mathrm{d}x`)
+			: math(`\\int \\frac{${fPrime}}{(${fx})^{${-n}}} \\; \\mathrm{d}x`);
 		let den = a2*(n+1);
 		let multiple = 1;
 		if (den < 0){
@@ -104,7 +104,7 @@ function case2Gen(case2: number, a2: number, b2: number, n: number): [string, st
 	// case 2: trig
 	const fx = a2===0 ? `\\sin` : `\\cos`;
 	const fPrime = a2===0 ? `\\cos x` : `\\sin x`;
-	const qn = math(`${fPrime} ${fx}^{${n}} x`);
+	const qn = math(`\\int ${fPrime} ${fx}^{${n}} x \\; \\mathrm{d}x`);
 	const multiple = a2===0 ? 1 : -1;
 	const ansTerm = new Term(multiple, `\\frac{${fx}^{${n+1}} x}{${n+1}}`);
 	const ans = math(`${ansTerm} + c`);
@@ -120,7 +120,7 @@ function case3Gen(case3: number, a3: number, b3: number, signCase3: number): [st
 			: signCase3===1 ? new Expression(xA, -b3)
 			: new Expression(b3, new Term(-1, xA));
 		const fPrime = a3===2 ? `x` : `x^{${a3-1}}`;
-		const qn = math(`${fPrime} e^{${fx}}`);
+		const qn = math(`\\int ${fPrime} \\; e^{${fx}} \\; \\mathrm{d}x`);
 		const multiple = signCase3===2 ? -1 : 1;
 		const ansTerm = new Term(multiple, `\\frac{e^{${fx}}}{${a3}}`);
 		const ans = math(`${ansTerm} + c`);
@@ -140,7 +140,7 @@ function case3Gen(case3: number, a3: number, b3: number, signCase3: number): [st
 		fx = `\\tan x`;
 		fPrime = `\\sec^2 x`;
 	}
-	const qn = math(`${fPrime} e^{${fx}}`);
+	const qn = math(`\\int ${fPrime} \\; e^{${fx}} \\; \\mathrm{d}x`);
 	const ansTerm = new Term(multiple, `e^{${fx}}`);
 	const ans = math(`${ansTerm} + c`);
 	return [qn, ans, ans, ''];
